b088f756277659524945ecac285ce7145f843ba0
Senthilvel Samatharman <s.senthilvel@gmail.com>
2019-12-20 11:09:12 +0530

    To transfer work to another workstation

--- app/static/eventhandler.js
+++ app/static/eventhandler.js
@@ -3,11 +3,31 @@ $(function () {
         e.stopPropagation();
         var currentEle = $(this);
         var value = $(this).html();
-        updateVal(currentEle, value);
+        var newEle = changeCurrentElementToInputBox(currentEle,value);
+        newEle.keyup(function(event){
+            if(event.keyCode == 13) {
+                saveChanges(newEle,currentEle);
+            }
+        }) 
+        $(document).click(function(){
+            saveChanges(newEle,currentEle);
+        })       
     });
 });
 
-function updateVal(currentEle, value) {
+function changeCurrentElementToInputBox(el, value)
+{
+    $(el).html('<input class="txtInput" type="text" value=' + value + '"/>');
+    $(".txtInput").focus();
+    return $(".txtInput");
+}
+
+function saveChanges(elNew, elCurrent)
+{
+    $(elCurrent).html(elNew.val().trim());
+}
+
+function updateVal(currentEle, value, id) {
     $(currentEle).html('<input class="thVal" type="text" value="' + value + '" />');
     $(".thVal").focus();
     $(".thVal").keyup(function (event) {
